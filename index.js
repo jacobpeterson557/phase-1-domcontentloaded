f43a546f397d936cafef45b1b43cb2602679bb51
+ <+>
2021-03-25 13:24:05 -0600

    commit tree 91d24a39d71c40c07de5ebcba15e292e775b11b1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const newText = document.querySelector('p')
+    newText.innerText = 'This is really cool!'
+})
+
